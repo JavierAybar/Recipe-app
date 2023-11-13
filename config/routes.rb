d6b82bee1000df1d53f96d4e5a0dcd2130102aea
SnakyMz <Soban.syeed@outlook.com>
2023-11-14 02:56:39 +0530

    Add foods controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
 
-  resources :users, only: [:index, :show] do 
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes
-    resources :foods
   end  
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/migrate/20231113155707_create_foods.rb
+++ db/migrate/20231113155707_create_foods.rb
@@ -2,7 +2,7 @@ class CreateFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :foods do |t|
       t.string :name
-      t.integer :measurement_unit, default: 0
+      t.string :measurement_unit
       t.decimal :price, default: 0
       t.integer :quantity, default: 0
 
