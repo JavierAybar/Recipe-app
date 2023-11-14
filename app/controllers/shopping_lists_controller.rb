b55aafdbb7aa3417bc8f2232b43e029aae7a2d1b
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-14 19:35:29 -0300

    Fix Linters

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,22 +1,20 @@
 class ShoppingListsController < ApplicationController
+  def index
+    @user = current_user
+    @total_foods = 0
+    @total_price = 0
+    @recipe_foods = []
 
-    def index
-      @user = current_user
-      @total_foods = 0
-      @total_price = 0
-      @recipe_foods = []
-  
-      @user.recipes.includes(recipe_foods: :food).each do |recipe|
-        recipe.recipe_foods.each do |recipe_food|
-          quantity = recipe_food.quantity
-          price = recipe_food.food.price * quantity
-  
-          @total_foods += 1
-          @total_price += price
-  
-          @recipe_foods << { food: recipe_food.food, quantity:, price: }
-        end
+    @user.recipes.includes(recipe_foods: :food).each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        quantity = recipe_food.quantity
+        price = recipe_food.food.price * quantity
+
+        @total_foods += 1
+        @total_price += price
+
+        @recipe_foods << { food: recipe_food.food, quantity:, price: }
       end
     end
-  
+  end
 end
